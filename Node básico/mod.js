62b57c19030dd13a897118a8aa82ddf69b54b804
Pedro Silva <pedro.o.silva975@gmail.com>
2025-02-20 21:48:05 -0300

    testes com module.exports

---
+++ Node básico/mod.js
@@ -0,0 +1,14 @@
+const nome = 'Pedro';
+const sobrenome = 'Henrique';
+const qualquercoisa = 'qualquercoisa';
+
+module.exports.AAA = qualquercoisa;
+module.exports.NOMEPESSOA = nome;
+module.exports.SOBRENOMEP = sobrenome;
+
+// module.exports.CHAVE = VALOR;
+// module.exports = { CHAVE, CHAVE, CHAVE }
+
+console.log(module.exports); 
+
+
